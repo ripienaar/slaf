5d34584423ba7cc6296e99aa694c814eb2bb5acb
R.I.Pienaar <rip@devco.net>
2010-04-09 13:59:08 +0000

    - handle comments in the config file - better variable names

--- lib/slaf/whitelist.rb
+++ lib/slaf/whitelist.rb
@@ -7,14 +7,16 @@ module SLAF
             @plugin_config = {}
 
             File.open("/etc/slaf/slaf.cfg") do |file|
-                while s = file.gets do
-                    if s =~ /^whitelist.file\s*=\s*(.+)/
+                while line = file.gets do
+                    next if line =~ /^#/
+
+                    if line =~ /^whitelist.file\s*=\s*(.+)/
                         @files.concat(Dir.glob($1.chomp))
-                    elsif s =~ /plugin.#{plugin}.whitelist.file\s*=\s*(.+)/
+                    elsif line =~ /plugin.#{plugin}.whitelist.file\s*=\s*(.+)/
                         @files.concat(Dir.glob($1.chomp))
-                    elsif s =~ /config.plugin.#{plugin}.(.*)\s*=\s*(.*)/ && plugin != ""
+                    elsif line =~ /config.plugin.#{plugin}.(.*)\s*=\s*(.*)/ && plugin != ""
                         @plugin_config[$1.to_sym] = $2.to_sym
-                    elsif s =~ /path\s*=\s*(.+)/
+                    elsif line =~ /path\s*=\s*(.+)/
                         @path = $1
                         ENV['PATH'] = @path
                     end
