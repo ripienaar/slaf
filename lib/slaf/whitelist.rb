1050985a69a831a92c20cd8f5f6c3b8ce5acf4a2
R.I.Pienaar <rip@devco.net>
2010-04-09 13:56:33 +0000

    be more forgiving about the use of whitespace in the config file

--- lib/slaf/whitelist.rb
+++ lib/slaf/whitelist.rb
@@ -8,13 +8,13 @@ module SLAF
 
             File.open("/etc/slaf/slaf.cfg") do |file|
                 while s = file.gets do
-                    if s =~ /^whitelist.file = (.+)/
+                    if s =~ /^whitelist.file\s*=\s*(.+)/
                         @files.concat(Dir.glob($1.chomp))
-                    elsif s =~ /plugin.#{plugin}.whitelist.file = (.+)/
+                    elsif s =~ /plugin.#{plugin}.whitelist.file\s*=\s*(.+)/
                         @files.concat(Dir.glob($1.chomp))
-                    elsif s =~ /config.plugin.#{plugin}.(.*) = (.*)/ && plugin != ""
+                    elsif s =~ /config.plugin.#{plugin}.(.*)\s*=\s*(.*)/ && plugin != ""
                         @plugin_config[$1.to_sym] = $2.to_sym
-                    elsif s =~ /path = (.+)/
+                    elsif s =~ /path\s*=\s*(.+)/
                         @path = $1
                         ENV['PATH'] = @path
                     end
