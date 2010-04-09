38dad72b3f86261541b4e17f3c3d133a28b4d44d
R.I.Pienaar <rip@devco.net>
2010-04-09 14:03:31 +0000

    add comments and make it a bit more readable

--- lib/slaf/whitelist.rb
+++ lib/slaf/whitelist.rb
@@ -1,4 +1,8 @@
 module SLAF
+    # Class to hold the whitelist, read config etc.
+    #
+    # This class holds all our config, it's not really whitelist specific
+    # anymore so might need to think about renaming it.
     class Whitelist
         attr_reader :files, :plugin_config, :path
 
@@ -12,10 +16,13 @@ module SLAF
 
                     if line =~ /^whitelist.file\s*=\s*(.+)/
                         @files.concat(Dir.glob($1.chomp))
+
                     elsif line =~ /plugin.#{plugin}.whitelist.file\s*=\s*(.+)/
                         @files.concat(Dir.glob($1.chomp))
+
                     elsif line =~ /config.plugin.#{plugin}.(.*)\s*=\s*(.*)/ && plugin != ""
                         @plugin_config[$1.to_sym] = $2.to_sym
+
                     elsif line =~ /path\s*=\s*(.+)/
                         @path = $1
                         ENV['PATH'] = @path
